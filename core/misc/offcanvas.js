e27d359825379040ad102a8bacda95b4fedb016b
Matthew Grill <matt.grill@acquia.com>
2016-07-12 10:41:30 -0700

    Added close animation and state tracking. Documentation.

--- core/misc/offcanvas.js
+++ core/misc/offcanvas.js
@@ -1,13 +1,13 @@
 /**
  * @file
- * Drupal's sidebar library.
+ * Drupal's Off Canvas library.
  */
 
 (function ($, Drupal) {
   'use strict';
 
   /**
-   * Command to open a dialog.
+   * Command to open an off canvas element.
    *
    * @param {Drupal.Ajax} ajax
    *   The Drupal Ajax object.
@@ -20,27 +20,52 @@
    *   Returns false if there was no selector property in the response object.
    */
   Drupal.AjaxCommands.prototype.openSidebar = function (ajax, response, status) {
+    // Set animation duration and get #page-wrapper width.
     var animationDuration = 600;
-    var pageWidth = $('#page-wrapper').width();
+    var $pageWrapper = $('#page-wrapper');
+    var pageWidth = $pageWrapper.width();
 
     var $page = $('#page');
-    var $sidebar = $('<div />', {
+
+    // Create elements used in offcanvas construction.
+    var $title = $('<h1 />', {text: 'My New Title'});
+    var $offcanvasContent = $('<div />', {class: 'content', html: 'My New Title'});
+    var $offcanvasClose = $('<span />', {class: 'offcanvasClose', text: 'x'}).click(function () {
+      $offcanvasWrapper.animate({right: -(pageWidth * .2)}, {duration: animationDuration, queue: false});
+      $page
+        .removeClass('offCanvasDisplay')
+        .animate({'margin-right': 0}, {duration: animationDuration, queue: false, complete: function () {
+          // Remove Off Canvas element, and set display state variable.
+          Drupal.offCanvas.visible = false;
+          $offcanvasWrapper.remove();
+        }});
+    });
+
+    // Construct Off Canvas wrapper
+    var $offcanvasWrapper = $('<div />', {
       id: 'offcanvas',
-      html: response.data,
       css: {
         width: pageWidth * .2,
         right: -(pageWidth * .2)
       }
-    });
-    $('#page-wrapper').append($sidebar);
+    }).append([$offcanvasClose, $title, $offcanvasContent]);
 
-    $sidebar
+    // Append Off Canvas wrapper to the 'page'
+    $pageWrapper.append($offcanvasWrapper);
+
+    // Animate $page and $offcanvasWrapper to simulate a slide in effect
+    $offcanvasWrapper
       .animate({right: 0}, {duration: animationDuration, queue: false});
     $page
       .addClass('offCanvasDisplay')
       .animate({
-        width: pageWidth * .8
-      }, {duration: animationDuration, queue: false});
+        'margin-right': pageWidth * .2
+      }, {duration: animationDuration, queue: false, complete: function () {
+        // Set the offCanvas visible state.
+        Drupal.offCanvas = {
+          visible: true
+        };
+      }});
 
     return false;
   };
