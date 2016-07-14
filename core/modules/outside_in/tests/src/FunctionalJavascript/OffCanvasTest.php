bbf7dd59b2af2edcd5949e4471407bc3468bfaa1
Ted Bowman <ted.git@tedbow.com>
2016-07-14 10:36:26 -0400

    Fix libraries file and renderer

--- core/modules/outside_in/tests/src/FunctionalJavascript/OffCanvasTest.php
+++ core/modules/outside_in/tests/src/FunctionalJavascript/OffCanvasTest.php
@@ -12,11 +12,12 @@ class OffCanvasTest extends JavascriptTestBase {
    */
   public static $modules = [
     'block',
+    'outside_in',
     'offcanvas_test',
   ];
 
   public function testOffCanvasLinks() {
-    $this->placeBlock('offcanvas_links_block');
+    $this->placeBlock('offcanvas_links_block', ['id' => 'offcanvaslinks']);
     $this->drupalGet('');
 
     $page = $this->getSession()->getPage();
@@ -24,6 +25,8 @@ class OffCanvasTest extends JavascriptTestBase {
     $web_assert = $this->assertSession();
     $web_assert->elementNotExists('css', '#offcanvas');
     $page->clickLink('Click Me 1!');
+    $condition = "(jQuery('#offcanvas').length > 0)";
+    $this->assertJsCondition($condition, 5000);
     $web_assert->elementExists('css', '#offcanvas');
 
   }
