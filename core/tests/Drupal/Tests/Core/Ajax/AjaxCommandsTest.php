337a8ea562612426bcd5e41e022e1e65379dd7a9
Ted Bowman <ted.git@tedbow.com>
2016-07-13 21:01:05 -0400

    Removed last changes made outside of outside_in module

--- core/tests/Drupal/Tests/Core/Ajax/AjaxCommandsTest.php
+++ core/tests/Drupal/Tests/Core/Ajax/AjaxCommandsTest.php
@@ -359,40 +359,6 @@ class AjaxCommandsTest extends UnitTestCase {
   }
 
   /**
-   * @covers \Drupal\Core\Ajax\OpenCanvasDialogCommand
-   */
-  public function testOpenCanvasDialogCommand() {
-    $command = $this->getMockBuilder('Drupal\Core\Ajax\OpenCanvasDialogCommand')
-      ->setConstructorArgs(array(
-        'Title', '<p>Text!</p>', array(
-          'url' => 'example',
-        ),
-      ))
-      ->setMethods(array('getRenderedContent'))
-      ->getMock();
-
-    // This method calls the render service, which isn't available. We want it
-    // to do nothing so we mock it to return a known value.
-    $command->expects($this->once())
-      ->method('getRenderedContent')
-      ->willReturn('rendered content');
-
-    $expected = array(
-      'command' => 'openOffCanvas',
-      'selector' => '#drupal-offcanvas',
-      'settings' => NULL,
-      'data' => 'rendered content',
-      'dialogOptions' => array(
-        'url' => 'example',
-        'title' => 'Title',
-        'modal' => FALSE,
-      ),
-    );
-    $this->assertEquals($expected, $command->render());
-  }
-
-
-  /**
    * @covers \Drupal\Core\Ajax\CloseModalDialogCommand
    */
   public function testCloseModalDialogCommand() {
