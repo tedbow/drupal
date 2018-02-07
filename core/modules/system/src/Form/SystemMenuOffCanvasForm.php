438a3dbe79f5fcd5e7cc8a60ff0c62350fc57650
Ted Bowman <ted+git@tedbow.com>
2018-02-07 12:16:12 -0500

    162223-65_8_5__8_6_reroll.patch

--- core/modules/system/src/Form/SystemMenuOffCanvasForm.php
+++ core/modules/system/src/Form/SystemMenuOffCanvasForm.php
@@ -98,7 +98,7 @@ class SystemMenuOffCanvasForm extends PluginFormBase implements ContainerInjecti
       '#type' => 'details',
       '#title' => $this->t('Edit menu %label', ['%label' => $this->menu->label()]),
       '#open' => TRUE,
-      '#access' => AccessResult::allowedIf(!$this->hasMenuOverrides()),
+      '#access' => !$this->hasMenuOverrides() && $this->menu->access('edit'),
     ];
     $form['entity_form'] += $this->getEntityForm($this->menu)->buildForm([], $form_state);
 
