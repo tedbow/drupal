925bf8a851d7e65c77ee5f4dcc6863ea15acec40
Ted Bowman <ted@tedbow.com>
2018-05-16 10:01:03 -0400

    https://www.drupal.org/files/issues/2018-05-16/interdiff_87-91.txt

--- core/modules/layout_builder/tests/src/FunctionalJavascript/InlineBlockContentBlockTest.php
+++ core/modules/layout_builder/tests/src/FunctionalJavascript/InlineBlockContentBlockTest.php
@@ -227,18 +227,7 @@ class InlineBlockContentBlockTest extends JavascriptTestBase {
 
     $this->drupalGet('node/1/layout');
 
-    $page->clickLink('Add Block');
-    $assert_session->assertWaitOnAjaxRequest();
-    $assert_session->elementExists('css', '.block-categories details:contains(Create new block)');
-    $this->clickLink('Basic block');
-    $assert_session->assertWaitOnAjaxRequest();
-    $assert_session->fieldValueEquals('Title', '');
-    $page->findField('Title')->setValue('Block title');
-    $textarea = $assert_session->elementExists('css', '[name="settings[block_form][body][0][value]"]');
-    $textarea->setValue('The block body');
-    $page->pressButton('Add Block');
-    $assert_session->assertWaitOnAjaxRequest();
-    $assert_session->pageTextContains('The block body');
+    $this->addInlineBlockToLayout('Block title', 'The block body');
 
     $this->assertSaveLayout();
     $this->drupalGet('node/1');
@@ -280,22 +269,9 @@ class InlineBlockContentBlockTest extends JavascriptTestBase {
       $this->assertEquals($block->get('body')->getValue()[0]['value'], 'The block body');
     }
     else {
-      // When reverting the original block body does not appear on the page.
+      // The block should not be visible.
+      // Blocks are currently only deleted when the parent entity is deleted.
       $assert_session->pageTextNotContains('The block body');
-      // When reverting to defaults any inline blocks used only in the
-      // override should be deleted.
-      // @todo Add test coverage for an inline block used in a default layout
-      // and an override. When reverting to defaults if the block is also in the
-      // default don't delete the content block.
-      // Also if the content block was first created in the default then also
-      // used different override then the content block may be used any other
-      // override for this bundle.
-      // So the only way to be sure the content block isn't referenced in any
-      // other override for this bundle you would have to check every other
-      // entity for this bundle.
-      // It may be better when creating an override to duplicate any content
-      // blocks. Then we could safely delete any content block in an override.
-      $this->assertEmpty($blocks);
     }
   }
 
@@ -339,23 +315,13 @@ class InlineBlockContentBlockTest extends JavascriptTestBase {
     $this->drupalGet('node/1/layout');
 
     // Add a inline block.
-    $page->clickLink('Add Block');
-    $assert_session->assertWaitOnAjaxRequest();
-    $assert_session->elementExists('css', '.block-categories details:contains(Create new block)');
-    $this->clickLink('Basic block');
-    $assert_session->assertWaitOnAjaxRequest();
-    $assert_session->fieldValueEquals('Title', '');
-    $page->findField('Title')->setValue('Block title');
-    $textarea = $assert_session->elementExists('css', '[name="settings[block_form][body][0][value]"]');
-    $textarea->setValue('The DEFAULT block body');
-    $page->pressButton('Add Block');
-    $assert_session->assertWaitOnAjaxRequest();
-    $assert_session->elementTextContains('css', '.block-inline-block-contentbasic', 'The DEFAULT block body');
+    $this->addInlineBlockToLayout('Block title', 'The DEFAULT block body');
 
     $this->assertSaveLayout();
     $this->drupalGet('node/1');
 
     $assert_session->pageTextContains('The DEFAULT block body');
+
 
     // Create a new revision.
     $this->drupalGet('node/1/edit');
@@ -401,10 +367,13 @@ class InlineBlockContentBlockTest extends JavascriptTestBase {
     /** @var \Drupal\Core\Cron $cron */
     $cron = \Drupal::service('cron');
     $this->drupalLogin($this->drupalCreateUser([
+      'administer content types',
       'access contextual links',
       'configure any layout',
       'administer node display',
       'administer node fields',
+      'administer nodes',
+      'bypass node access',
     ]));
     $assert_session = $this->assertSession();
     $page = $this->getSession()->getPage();
@@ -453,7 +422,6 @@ class InlineBlockContentBlockTest extends JavascriptTestBase {
     // Ensure other blocks still exist.
     $this->assertCount(2, BlockContent::loadMultiple());
 
-
     $this->drupalGet('node/1/layout');
     $assert_session->pageTextContains('The DEFAULT block body');
 
@@ -468,10 +436,10 @@ class InlineBlockContentBlockTest extends JavascriptTestBase {
     $this->assertCount(2, BlockContent::loadMultiple());*/
 
     // Ensure content block is deleted when node is deleted.
-    Node::load(1)->delete();
+    $this->drupalGet('node/1/delete');
+    $page->pressButton('Delete');
     $this->assertEmpty(Node::load(1));
     $cron->run();
-    \Drupal::entityTypeManager()->getStorage('block_content')->resetCache([$node_1_block_id]);
     $this->assertEmpty(BlockContent::load($node_1_block_id));
     $this->assertCount(1, BlockContent::loadMultiple());
 
@@ -486,14 +454,16 @@ class InlineBlockContentBlockTest extends JavascriptTestBase {
     $this->assertCount(2, BlockContent::loadMultiple());
 
     // Delete the other node so bundle can be deleted.
-    Node::load(2)->delete();
+    $this->drupalGet('node/2/delete');
+    $page->pressButton('Delete');
+    $this->assertEmpty(Node::load(2));
     $cron->run();
     // Ensure content block was deleted.
     $this->assertEmpty(BlockContent::load($node_2_block_id));
     $this->assertCount(1, BlockContent::loadMultiple());
 
     // Delete the bundle which has the default layout.
-    $this->drupalGet('admin/structure/types/manage/bundle_with_section_fields/delete');
+    $this->drupalGet("$field_ui_prefix/delete");
     $page->pressButton('Delete');
     $cron->run();
 
