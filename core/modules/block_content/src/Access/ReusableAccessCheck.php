3d5004604f354f3daf951c81b65296815ca0ae37
Ted Bowman <ted@tedbow.com>
2018-05-21 16:04:33 -0400

    service check works

--- core/modules/block_content/src/Access/ReusableAccessCheck.php
+++ core/modules/block_content/src/Access/ReusableAccessCheck.php
@@ -3,38 +3,26 @@
 namespace Drupal\block_content\Access;
 
 use Drupal\block_content\BlockContentInterface;
+use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Routing\Access\AccessInterface;
-use Drupal\Core\Routing\RouteMatchInterface;
-use Drupal\Core\Session\AccountInterface;
 
+/**
+ * Route access check for 'block_content' entities based on 'reusable' field.
+ */
 class ReusableAccessCheck implements AccessInterface {
 
   /**
-   * Checks routing access to the layout.
+   * Checks routing access to a block.
    *
-   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
-   *   The current route match.
-   * @param \Drupal\Core\Session\AccountInterface $account
-   *   The currently logged in account.
+   * @param \Drupal\block_content\BlockContentInterface $block_content
+   *   The block content entity.
    *
    * @return \Drupal\Core\Access\AccessResultInterface
    *   The access result.
    */
-  public function access(RouteMatchInterface $route_match, AccountInterface $account) {
-    $section_storage = $route_match->getParameter('section_storage');
-
-    if (empty($section_storage)) {
-      return AccessResult::forbidden()->addCacheContexts(['route']);
-    }
-
-    if (!$section_storage instanceof SectionStorageInterface) {
-      $access = AccessResult::forbidden();
-    }
-    else {
-      $access = AccessResult::allowedIfHasPermission($account, 'configure any layout');
-    }
-
-    return $access->addCacheableDependency($section_storage);
+  public function access(BlockContentInterface $block_content) {
+    $access = AccessResult::allowedIf($block_content->isReusable());
+    return $access->addCacheableDependency($block_content);
   }
 
 }
