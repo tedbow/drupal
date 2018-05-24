fe64dd73eea8ab784cafddbc45321f6ed9bd6cd8
Ted Bowman <ted@tedbow.com>
2018-05-24 14:10:57 -0400

    Update block access to handle dependees

--- core/modules/block_content/src/BlockContentAccessControlHandler.php
+++ core/modules/block_content/src/BlockContentAccessControlHandler.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\block_content;
 
+use Drupal\Core\Access\AccessDependentInterface;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityAccessControlHandler;
@@ -19,6 +20,18 @@ class BlockContentAccessControlHandler extends EntityAccessControlHandler {
    */
   protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
     if ($operation === 'view') {
+      /** @var \Drupal\block_content\BlockContentInterface $entity */
+      if (!$entity->isReusable()) {
+        if (!$entity instanceof AccessDependentInterface) {
+          throw new \Exception("what?");
+        }
+        if (empty($entity->getAccessDependees())) {
+          return AccessResult::forbidden("None set");
+        }
+        if (!$entity->dependeeAccess($operation, $account)) {
+          return AccessResult::forbidden('no access to parent');
+        }
+      }
       return AccessResult::allowedIf($entity->isPublished())->addCacheableDependency($entity)
         ->orIf(AccessResult::allowedIfHasPermission($account, 'administer blocks'));
     }
