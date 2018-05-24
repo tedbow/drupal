b60163c737a6510e021ee4c7cea5efcc33e87a9a
Ted Bowman <ted@tedbow.com>
2018-05-24 14:32:49 -0400

    _wip access trait

---
+++ core/lib/Drupal/Core/Access/AccessDependentInterface.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\Core\Access;
+
+use Drupal\Core\Session\AccountInterface;
+
+interface AccessDependentInterface {
+
+  /**
+   * @param \Drupal\Core\Access\AccessibleInterface $access_dependee
+   */
+  public function setAccessDependees(AccessibleInterface $access_dependee);
+
+  /**
+   * Gets the access dependee.
+   *
+   * @return \Drupal\Core\Access\AccessibleInterface
+   *   The access dependee.
+   */
+  public function getAccessDependees();
+
+
+  //***
+   * Checks data value access.
+   *
+   * @param string $operation
+   *   The operation to be performed.
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   (optional) The user for which to check access, or NULL to check access
+   *   for the current user. Defaults to NULL.
+   * @param bool $return_as_object
+   *   (optional) Defaults to FALSE.
+   *
+   * @return bool|\Drupal\Core\Access\AccessResultInterface
+   *   The access result. Returns a boolean if $return_as_object is FALSE (this
+   *   is the default) and otherwise an AccessResultInterface object.
+   *   When a boolean is returned, the result of AccessInterface::isAllowed() is
+   *   returned, i.e. TRUE means access is explicitly allowed, FALSE means
+   *   access is either explicitly forbidden or "no opinion".
+   */
+  public function dependeeAccess($operation, AccountInterface $account = NULL, $return_as_object = FALSE);*/
+
+}
