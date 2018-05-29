e29a2eb8e84e912cb2f30a4e0af253025f260954
Ted Bowman <ted@tedbow.com>
2018-05-29 16:48:32 -0400

    update access handler

--- core/modules/block_content/src/BlockContentAccessControlHandler.php
+++ core/modules/block_content/src/BlockContentAccessControlHandler.php
@@ -19,9 +19,9 @@ class BlockContentAccessControlHandler extends EntityAccessControlHandler {
    * {@inheritdoc}
    */
   protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
+    $dependee_access = NULL;
     /** @var \Drupal\block_content\BlockContentInterface $entity */
     if (!$entity->isReusable()) {
-      $dependee_access = NULL;
       if (!$entity instanceof AccessDependentInterface) {
         throw new \Exception("Non-reusable block entities must implement \Drupal\Core\Access\AccessDependentInterface for access control.");
       }
@@ -30,14 +30,20 @@ class BlockContentAccessControlHandler extends EntityAccessControlHandler {
         return AccessResult::forbidden("Non-reusable blocks must set an access dependee for access control.")->addCacheableDependency($dependee);
       }
       $dependee_access = $dependee->access($operation, $account, TRUE);
+    }
+    if ($operation === 'view') {
       $access = AccessResult::allowedIf($entity->isPublished())->addCacheableDependency($entity)
         ->orIf(AccessResult::allowedIfHasPermission($account, 'administer blocks'));
-      if ($dependee_access) {
-        $access->andIf($dependee_access);
-      }
-      return $access;
+
     }
-    return parent::checkAccess($entity, $operation, $account);
+    else {
+      $access = parent::checkAccess($entity, $operation, $account)->andIf($dependee_access);
+    }
+
+    if ($dependee_access) {
+      $access->andIf($dependee_access);
+    }
+    return $access;
   }
 
 }
