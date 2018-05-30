4d9e90789dcc092fc29a1fdd47c3a178f979c2ad
Ted Bowman <ted@tedbow.com>
2018-05-30 13:47:46 -0400

    remove changes now in issues 2976366 and 2976334

--- core/modules/block_content/src/BlockContentAccessControlHandler.php
+++ core/modules/block_content/src/BlockContentAccessControlHandler.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\block_content;
 
-use Drupal\Core\Access\AccessDependentInterface;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityAccessControlHandler;
@@ -20,24 +19,10 @@ class BlockContentAccessControlHandler extends EntityAccessControlHandler {
    */
   protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
     if ($operation === 'view') {
-      $access = AccessResult::allowedIf($entity->isPublished())->addCacheableDependency($entity)
+      return AccessResult::allowedIf($entity->isPublished())->addCacheableDependency($entity)
         ->orIf(AccessResult::allowedIfHasPermission($account, 'administer blocks'));
     }
-    else {
-      $access = parent::checkAccess($entity, $operation, $account);
-    }
-    /** @var \Drupal\block_content\BlockContentInterface $entity */
-    if ($entity->isReusable() === FALSE) {
-      if (!$entity instanceof AccessDependentInterface) {
-        throw new \Exception("Non-reusable block entities must implement \Drupal\Core\Access\AccessDependentInterface for access control.");
-      }
-      $dependee = $entity->getAccessDependee();
-      if (empty($dependee)) {
-        return AccessResult::forbidden("Non-reusable blocks must set an access dependee for access control.")->addCacheableDependency($dependee);
-      }
-      $access->andIf($dependee->access($operation, $account, TRUE));
-    }
-    return $access;
+    return parent::checkAccess($entity, $operation, $account);
   }
 
 }
