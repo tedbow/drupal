f51003cf3b785dd6ed53f9416921def061b6870c
Ted Bowman <ted@tedbow.com>
2018-05-30 13:51:15 -0400

    https://www.drupal.org/files/issues/2018-05-30/2976366-2.patch

---
+++ core/lib/Drupal/Core/Entity/EntityUsageBase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\Core\Entity;
+
+/**
+ * Base class for entity usage service.
+ */
+abstract class EntityUsageBase implements EntityUsageInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addByEntities(EntityInterface $child_entity, EntityInterface $parent_entity, $count = 1) {
+    $this->add($child_entity->getEntityTypeId(), $child_entity->id(), $parent_entity->getEntityTypeId(), $parent_entity->id());
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function deleteByChildEntity(EntityInterface $child_entity) {
+    $this->deleteMultiple($child_entity->getEntityTypeId(), [$child_entity->id()]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function removeByEntities(EntityInterface $child_entity, EntityInterface $parent_entity, $count = 1) {
+    return $this->remove($child_entity->getEntityTypeId(), $child_entity->id(), $parent_entity->getEntityTypeId(), $parent_entity->id());
+  }
+
+}
