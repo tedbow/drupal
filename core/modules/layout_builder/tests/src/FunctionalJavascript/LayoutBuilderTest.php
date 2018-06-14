ca95f471975d71d16bee81971f49733bf728e6ce
Ted Bowman <ted@tedbow.com>
2018-06-14 10:10:38 -0400

    test passing

--- core/modules/layout_builder/tests/src/FunctionalJavascript/LayoutBuilderTest.php
+++ core/modules/layout_builder/tests/src/FunctionalJavascript/LayoutBuilderTest.php
@@ -36,26 +36,13 @@ class LayoutBuilderTest extends JavascriptTestBase {
     //   https://www.drupal.org/project/drupal/issues/2917777.
     $this->drupalPlaceBlock('local_tasks_block');
 
-    // Create two nodes.
     $this->createContentType(['type' => 'bundle_with_section_field']);
     $this->createNode([
       'type' => 'bundle_with_section_field',
       'title' => 'The first node title',
       'body' => [
         [
-          // Create a link that should be disabled in the Layout Builder.
-          'value' => '<a href="/search/node">Take me away</a>',
-          'format' => 'full_html',
-        ],
-      ],
-    ]);
-    $this->createNode([
-      'type' => 'bundle_with_section_field',
-      'title' => 'The second node title',
-      'body' => [
-        [
-          'value' => 'The second node body',
-          'format' => 'full_html',
+          'value' => 'Node body',
         ],
       ],
     ]);
@@ -76,12 +63,17 @@ class LayoutBuilderTest extends JavascriptTestBase {
         'value' => '<a href="/search/node">Take me away</a>',
         'format' => 'full_html',
       ],
-    ]);
+    ])->save();
   }
 
+  /**
+   * Tests that forms and links are disabled in the Layout Builder preview.
+   *
+   * @throws \Behat\Mink\Exception\ElementNotFoundException
+   * @throws \Behat\Mink\Exception\ExpectationException
+   */
   public function testFormsLinksDisabled() {
     $assert_session = $this->assertSession();
-    $page = $this->getSession()->getPage();
 
     $this->drupalLogin($this->drupalCreateUser([
       'configure any layout',
@@ -92,67 +84,80 @@ class LayoutBuilderTest extends JavascriptTestBase {
 
     $field_ui_prefix = 'admin/structure/types/manage/bundle_with_section_field';
 
-    // From the manage display page, go to manage the layout.
-    $this->drupalGet("$field_ui_prefix/display/default");
-    $assert_session->linkExists('Manage layout');
-    $this->clickLink('Manage layout');
-    $assert_session->addressEquals("$field_ui_prefix/display-layout/default");
+    $this->drupalGet("$field_ui_prefix/display-layout/default");
+
+    // Add a block with a link and one with a form.
+    $this->addBlock('Block with link', '.block-field-blockblock-contentbasicbody');
     $this->addBlock('Search form', '.search-block-form');
 
-      $page->pressButton('Search');
-      // Ensure we didn't get redirected to the search page.
-      $this->assertEmpty($assert_session->waitForElement('css', '.search-form'));
-    $assert_session->addressEquals("$field_ui_prefix/display-layout/default");
-
-    $this->addBlock('Block with link', 'block-field-blockblock-contentbasicbody');
-
-
-    $assert_session->linkExists('Take me away');
-    $page->clickLink('Take me away');
-    // Ensure we didn't get redirected to the search page.
-    $this->assertEmpty($assert_session->waitForElement('css', '.search-form'));
-    $assert_session->addressEquals("$field_ui_prefix/display-layout/default");
+    // Ensure the links and forms are disable using the defaults.
+    $this->assertLinksFormDisabled();
 
     $this->clickLink('Save Layout');
     $assert_session->addressEquals("$field_ui_prefix/display/default");
 
     // Enable overrides.
     $this->drupalPostForm("$field_ui_prefix/display/default", ['layout[allow_custom]' => TRUE], 'Save');
-    $this->drupalGet('node/1');
+    $this->drupalGet('node/1/layout');
 
-    // Remove the section from the defaults.
-    $assert_session->linkExists('Layout');
-    $this->clickLink('Layout');
-
-    // Wait for block form to be rendered in the Layout Builder.
+    // Wait for search form to be rendered in the Layout Builder.
     $this->assertNotEmpty($assert_session->waitForElement('css', '.search-block-form'));
-    file_put_contents('/Users/ted.bowman/Sites/www/test.html', $page->getOuterHtml());
-    $assert_session->linkExists('Take me away');
-    // Ensure we didn't get redirected to the search page.
-    $this->assertEmpty($assert_session->waitForElement('css', '.search-form'));
-    $assert_session->addressEquals("node/1/layout");
+    // Ensure the links and forms are also disabled in using the override.
+    $this->assertLinksFormDisabled();
   }
 
   /**
-   * @param $assert_session
-   * @param $page
+   * Adds a block in the Layout Builder.
+   *
+   * @param string $block_link_text
+   *   The link text to add the block.
+   * @param string $rendered_locator
+   *   The CSS locator to confirm the block was rendered.
+   *
+   * @throws \Behat\Mink\Exception\ElementNotFoundException
+   * @throws \Behat\Mink\Exception\ExpectationException
    */
-  protected function addBlock($block_link_test, $rendered_locator) {
+  protected function addBlock($block_link_text, $rendered_locator) {
     $assert_session = $this->assertSession();
     $page = $this->getSession()->getPage();
     // Add a new block.
     $assert_session->linkExists('Add Block');
     $this->clickLink('Add Block');
+
     $this->assertNotEmpty($assert_session->waitForElementVisible('css', '#drupal-off-canvas'));
-    $assert_session->linkExists($block_link_test);
-    $this->clickLink($block_link_test);
-    // Wait for off-cavnas dialog to reopen with block form.
+    $assert_session->assertWaitOnAjaxRequest();
+
+    $assert_session->linkExists($block_link_text);
+    $this->clickLink($block_link_text);
+    // Wait for off-canvas dialog to reopen with block form.
     $this->assertNotEmpty($assert_session->waitForElementVisible('css', ".layout-builder-add-block"));
     $assert_session->assertWaitOnAjaxRequest();
     $page->pressButton('Add Block');
 
     // Wait for block form to be rendered in the Layout Builder.
     $this->assertNotEmpty($assert_session->waitForElement('css', $rendered_locator));
+  }
+
+  /**
+   * Asserts that forms and links added inside the Layout Builder are disabled.
+   *
+   * @throws \Behat\Mink\Exception\ElementNotFoundException
+   * @throws \Behat\Mink\Exception\ExpectationException
+   */
+  protected function assertLinksFormDisabled() {
+    $address = $this->getUrl();
+    $assert_session = $this->assertSession();
+    $page = $this->getSession()->getPage();
+    $page->pressButton('Search');
+    // Ensure we didn't get redirected to the search page.
+    $this->assertEmpty($assert_session->waitForElement('css', '.search-form'));
+    $assert_session->addressEquals($address);
+
+    $assert_session->linkExists('Take me away');
+    $page->clickLink('Take me away');
+    // Ensure we didn't get redirected to the search page.
+    $this->assertEmpty($assert_session->waitForElement('css', '.search-form'));
+    $assert_session->addressEquals($address);
   }
 
 }
