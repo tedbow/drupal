fcfe9df63353b1d2cd7560aea1165342999d4869
Ted Bowman <ted@tedbow.com>
2018-06-19 15:24:36 -0400

    Patch https://www.drupal.org/files/issues/2018-06-13/2957425-149_plus_2976334-33.patch

---
+++ core/modules/block_content/tests/src/Kernel/BlockContentEntityReferenceSelectionTest.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Drupal\Tests\block_content\Kernel;
+
+use Drupal\block_content\Entity\BlockContent;
+use Drupal\block_content\Entity\BlockContentType;
+use Drupal\block_content_test\Plugin\EntityReferenceSelection\TestSelection;
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests EntityReference selection handlers don't return non-reusable blocks.
+ *
+ * @see block_content_query_block_content_access_alter()
+ *
+ * @group block_content
+ */
+class BlockContentEntityReferenceSelectionTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'block',
+    'block_content',
+    'block_content_test',
+    'system',
+    'user',
+  ];
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+    $this->installSchema('system', ['sequence']);
+    $this->installEntitySchema('user');
+    $this->installEntitySchema('block_content');
+
+    // Create a block content type.
+    $block_content_type = BlockContentType::create([
+      'id' => 'spiffy',
+      'label' => 'Mucho spiffy',
+      'description' => "Provides a block type that increases your site's spiffiness by up to 11%",
+    ]);
+    $block_content_type->save();
+    $this->entityTypeManager = $this->container->get('entity_type.manager');
+  }
+
+  /**
+   * Tests that non-reusable blocks are not referenceable entities.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Exception
+   */
+  public function testReferenceableEntities() {
+    // And reusable and non-reusable block content entities.
+    $block_content_reusable = BlockContent::create([
+      'info' => 'Reusable Block',
+      'type' => 'spiffy',
+      'reusable' => TRUE,
+    ]);
+    $block_content_reusable->save();
+    $block_content_nonreusable = BlockContent::create([
+      'info' => 'Non-reusable Block',
+      'type' => 'spiffy',
+      'reusable' => FALSE,
+    ]);
+    $block_content_nonreusable->save();
+
+    // Ensure that queries without all the tags are not altered.
+    $query = $this->entityTypeManager->getStorage('block_content')->getQuery();
+    $this->assertCount(2, $query->execute());
+
+    $query = $this->entityTypeManager->getStorage('block_content')->getQuery();
+    $query->addTag('block_content_access');
+    $this->assertCount(2, $query->execute());
+
+    $query = $this->entityTypeManager->getStorage('block_content')->getQuery();
+    $query->addTag('entity_query_block_content');
+    $this->assertCount(2, $query->execute());
+
+    // Use \Drupal\Core\Entity\Plugin\EntityReferenceSelection\DefaultSelection
+    // class to test that getReferenceableEntities() does not get the
+    // non-reusable entity.
+    $configuration = [
+      'target_type' => 'block_content',
+      'target_bundles' => ['spiffy' => 'spiffy'],
+      'sort' => ['field' => '_none'],
+    ];
+    $selection_handler = new TestSelection($configuration, '', '', $this->container->get('entity.manager'), $this->container->get('module_handler'), \Drupal::currentUser());
+    // Setup the 3 expectation cases.
+    $both_blocks = [
+      'spiffy' => [
+        $block_content_reusable->id() => $block_content_reusable->label(),
+        $block_content_nonreusable->id() => $block_content_nonreusable->label(),
+      ],
+    ];
+    $reusable_block = ['spiffy' => [$block_content_reusable->id() => $block_content_reusable->label()]];
+    $non_reusable_block = ['spiffy' => [$block_content_nonreusable->id() => $block_content_nonreusable->label()]];
+
+    $this->assertEquals(
+      $reusable_block,
+      $selection_handler->getReferenceableEntities()
+    );
+
+    // Test various ways in which an EntityReferenceSelection plugin could set
+    // the 'reusable' condition. If the plugin has set a condition on 'reusable'
+    // at all then 'block_content_query_entity_reference_alter()' will not set
+    // a reusable condition.
+    $selection_handler->setTestMode('reusable_condition_false');
+    $this->assertEquals(
+      $non_reusable_block,
+      $selection_handler->getReferenceableEntities()
+    );
+
+    $selection_handler->setTestMode('reusable_condition_exists');
+    $this->assertEquals(
+      $both_blocks,
+      $selection_handler->getReferenceableEntities()
+    );
+
+    $selection_handler->setTestMode('reusable_condition_group_false');
+    $this->assertEquals(
+      $non_reusable_block,
+      $selection_handler->getReferenceableEntities()
+    );
+
+    $selection_handler->setTestMode('reusable_condition_group_true');
+    $this->assertEquals(
+      $reusable_block,
+      $selection_handler->getReferenceableEntities()
+    );
+
+    $selection_handler->setTestMode('reusable_condition_nested_group_false');
+    $this->assertEquals(
+      $non_reusable_block,
+      $selection_handler->getReferenceableEntities()
+    );
+
+    $selection_handler->setTestMode('reusable_condition_nested_group_true');
+    $this->assertEquals(
+      $reusable_block,
+      $selection_handler->getReferenceableEntities()
+    );
+
+    // Change the block to reusable.
+    $block_content_nonreusable->setReusable(TRUE);
+    $block_content_nonreusable->save();
+    // Don't use any conditions.
+    $selection_handler->setTestMode(NULL);
+    // Ensure that the block is now returned as a referenceable entity.
+    $this->assertEquals(
+      $both_blocks,
+      $selection_handler->getReferenceableEntities()
+    );
+  }
+
+}
