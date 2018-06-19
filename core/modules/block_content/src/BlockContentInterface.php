423a95e766dd4db96accdbf3aba806502caa2e39
Ted Bowman <ted@tedbow.com>
2018-06-19 15:24:36 -0400

    Patch https://www.drupal.org/files/issues/2018-06-13/2957425-149_plus_2976334-33.patch

--- core/modules/block_content/src/BlockContentInterface.php
+++ core/modules/block_content/src/BlockContentInterface.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\block_content;
 
+use Drupal\Core\Access\AccessDependentInterface;
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityChangedInterface;
 use Drupal\Core\Entity\EntityPublishedInterface;
@@ -10,7 +11,7 @@ use Drupal\Core\Entity\RevisionLogInterface;
 /**
  * Provides an interface defining a custom block entity.
  */
-interface BlockContentInterface extends ContentEntityInterface, EntityChangedInterface, RevisionLogInterface, EntityPublishedInterface {
+interface BlockContentInterface extends ContentEntityInterface, EntityChangedInterface, RevisionLogInterface, EntityPublishedInterface, AccessDependentInterface {
 
   /**
    * Returns the block revision log message.
@@ -47,6 +48,24 @@ interface BlockContentInterface extends ContentEntityInterface, EntityChangedInt
    *   \Drupal\Core\Entity\RevisionLogInterface::setRevisionLogMessage() instead.
    */
   public function setRevisionLog($revision_log);
+
+  /**
+   * Determines if the block is reusable or not.
+   *
+   * @return bool
+   *   Returns TRUE if reusable and FALSE otherwise.
+   */
+  public function isReusable();
+
+  /**
+   * Sets the block to be reusable.
+   *
+   * @param bool $reusable
+   *   Whether the block should be reusable, defaults to TRUE.
+   *
+   * @return $this
+   */
+  public function setReusable($reusable = TRUE);
 
   /**
    * Sets the theme value.
