9a2a6e2ad12220ef1c9633ffc508db1868e64761
Ted Bowman <ted@tedbow.com>
2018-06-25 09:35:35 -0400

    post_update use config updater service.

--- core/modules/block_content/block_content.post_update.php
+++ core/modules/block_content/block_content.post_update.php
@@ -5,35 +5,42 @@
  * Post update functions for Custom Block.
  */
 
+use Drupal\Core\Config\Entity\ConfigEntityUpdater;
+
 /**
  * Adds 'has_parent' filter to Custom Block views.
  */
-function block_content_post_update_add_views_parent_filter() {
-  $config_factory = \Drupal::configFactory();
+function block_content_post_update_add_views_parent_filter(&$sandbox = NULL) {
   $data_table = \Drupal::entityTypeManager()
     ->getDefinition('block_content')
     ->getDataTable();
 
-  foreach ($config_factory->listAll('views.view.') as $view_config_name) {
-    $view = $config_factory->getEditable($view_config_name);
+  \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'view', function ($view) use ($data_table) {
+    /** @var \Drupal\views\ViewEntityInterface $view */
     if ($view->get('base_table') != $data_table) {
-      continue;
+      return FALSE;
     }
-    foreach ($view->get('display') as $display_name => $display) {
+    $save_view = FALSE;
+    $displays = $view->get('display');
+    foreach ($displays as $display_name => &$display) {
       // Update the default display and displays that have overridden filters.
       if (!isset($display['display_options']['filters']['has_parent']) &&
         ($display_name === 'default' || isset($display['display_options']['filters']))) {
-        // Save off the base part of the config path we are updating.
-        $base = "display.$display_name.display_options.filters.has_parent";
-        $view->set("$base.id", 'has_parent')
-          ->set("$base.plugin_id", 'boolean_string')
-          ->set("$base.table", $data_table)
-          ->set("$base.field", "has_parent")
-          ->set("$base.value", '0')
-          ->set("$base.entity_type", "block_content")
-          ->set("$base.entity_field", "parent_entity_type");
+        $display['display_options']['filters']['has_parent'] = [
+          'id' => 'has_parent',
+          'plugin_id' => 'boolean_string',
+          'table' => $data_table,
+          'field' => 'has_parent',
+          'value' => '0',
+          'entity_type' => 'block_content',
+          'entity_field' => 'parent_entity_type',
+        ];
+        $save_view = TRUE;
       }
     }
-    $view->save();
-  }
+    if ($save_view) {
+      $view->set('display', $displays);
+    }
+    return $save_view;
+  });
 }
