2fcdce4a32a6fa4f403c3471b0452da69526c51a
Ted Bowman <ted@tedbow.com>
2018-06-28 21:54:24 -0400

    try use context instead of access dependency

--- core/modules/layout_builder/src/Plugin/Derivative/InlineBlockContentDeriver.php
+++ core/modules/layout_builder/src/Plugin/Derivative/InlineBlockContentDeriver.php
@@ -4,13 +4,19 @@ namespace Drupal\layout_builder\Plugin\Derivative;
 
 use Drupal\Component\Plugin\Derivative\DeriverBase;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\Core\Plugin\Context\ContextDefinition;
 use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
+use Drupal\Core\Plugin\PluginBase;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Retrieves block plugin definitions for all custom block types.
  */
 class InlineBlockContentDeriver extends DeriverBase implements ContainerDeriverInterface {
+
+  use StringTranslationTrait;
 
   /**
    * The entity type manager service.
@@ -46,9 +52,21 @@ class InlineBlockContentDeriver extends DeriverBase implements ContainerDeriverI
     if ($this->entityTypeManager->hasDefinition('block_content_type')) {
       $block_content_types = $this->entityTypeManager->getStorage('block_content_type')->loadMultiple();
       foreach ($block_content_types as $id => $type) {
-        $this->derivatives[$id] = $base_plugin_definition;
-        $this->derivatives[$id]['admin_label'] = $type->label();
-        $this->derivatives[$id]['config_dependencies'][$type->getConfigDependencyKey()][] = $type->getConfigDependencyName();
+       // foreach ($this->entityTypeManager->getDefinitions() as $entity_type_id => $definition) {
+        //  if ($entity_type_id === 'entity_view_display' || in_array(FieldableEntityInterface::class, class_implements($definition->getClass()))) {
+            $derivative_id = $id . PluginBase::DERIVATIVE_SEPARATOR . $entity_type_id;
+            $derivative_id = $id;
+            $this->derivatives[$derivative_id] = $base_plugin_definition;
+            $this->derivatives[$derivative_id]['admin_label'] = $type->label();
+            $this->derivatives[$derivative_id]['config_dependencies'][$type->getConfigDependencyKey()][] = $type->getConfigDependencyName();
+            /*$this->derivatives[$derivative_id]['context'] = [
+              'entity' => new ContextDefinition('entity:' . $entity_type_id, $definition->getLabel(), TRUE),
+            ];*/
+
+          //}
+
+        //}
+
       }
     }
     return parent::getDerivativeDefinitions($base_plugin_definition);
