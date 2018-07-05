5e9cfaff86470723ed669a45fb8522ffd9ce1b6c
Ted Bowman <ted@tedbow.com>
2018-07-05 09:20:50 -0400

    review https://www.drupal.org/project/drupal/issues/2957425#comment-12674964

--- core/modules/layout_builder/src/InlineBlockContentUsage.php
+++ core/modules/layout_builder/src/InlineBlockContentUsage.php
@@ -15,16 +15,16 @@ class InlineBlockContentUsage {
    *
    * @var \Drupal\Core\Database\Connection
    */
-  protected $connection;
+  protected $database;
 
   /**
    * Creates an InlineBlockContentUsage object.
    *
-   * @param \Drupal\Core\Database\Connection $connection
+   * @param \Drupal\Core\Database\Connection $database
    *   The database connection.
    */
-  public function __construct(Connection $connection) {
-    $this->connection = $connection;
+  public function __construct(Connection $database) {
+    $this->database = $database;
   }
 
   /**
@@ -40,7 +40,7 @@ class InlineBlockContentUsage {
    * @throws \Exception
    */
   public function addUsage($block_content_id, $layout_entity_type, $layout_entity_id) {
-    $this->connection->merge('inline_block_content_usage')
+    $this->database->merge('inline_block_content_usage')
       ->keys([
         'block_content_id' => $block_content_id,
         'layout_entity_id' => $layout_entity_id,
@@ -58,7 +58,7 @@ class InlineBlockContentUsage {
    *   The entity IDs.
    */
   public function getUnused($limit = 100) {
-    $query = $this->connection->select('inline_block_content_usage', 't');
+    $query = $this->database->select('inline_block_content_usage', 't');
     $query->fields('t', ['block_content_id']);
     $query->isNull('layout_entity_id');
     $query->isNull('layout_entity_type');
@@ -72,7 +72,7 @@ class InlineBlockContentUsage {
    *   The layout entity.
    */
   public function removeByLayoutEntity(EntityInterface $entity) {
-    $query = $this->connection->update('inline_block_content_usage')
+    $query = $this->database->update('inline_block_content_usage')
       ->fields([
         'layout_entity_type' => NULL,
         'layout_entity_id' => NULL,
@@ -89,7 +89,7 @@ class InlineBlockContentUsage {
    *   The block content entity IDs.
    */
   public function deleteUsage(array $block_content_ids) {
-    $query = $this->connection->delete('inline_block_content_usage')->condition('block_content_id', $block_content_ids, 'IN');
+    $query = $this->database->delete('inline_block_content_usage')->condition('block_content_id', $block_content_ids, 'IN');
     $query->execute();
   }
 
@@ -103,7 +103,7 @@ class InlineBlockContentUsage {
    *   The usage record with properties layout_entity_id and layout_entity_type.
    */
   public function getUsage($block_content_id) {
-    $query = $this->connection->select('inline_block_content_usage');
+    $query = $this->database->select('inline_block_content_usage');
     $query->condition('block_content_id', $block_content_id);
     $query->fields('inline_block_content_usage', ['layout_entity_id', 'layout_entity_type']);
     $query->range(0, 1);
