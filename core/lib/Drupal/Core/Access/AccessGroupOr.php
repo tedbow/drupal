db8c7c72239dd87d0deb101fa73f6fd6da3a34ea
Ted Bowman <ted@tedbow.com>
2018-07-12 15:39:06 -0400

    move more logic into the base group class

--- core/lib/Drupal/Core/Access/AccessGroupOr.php
+++ core/lib/Drupal/Core/Access/AccessGroupOr.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\Core\Access;
 
-use Drupal\Core\Session\AccountInterface;
 
 /**
  * An access group where at least one dependencies must be allowed.
@@ -12,12 +11,8 @@ class AccessGroupOr extends AccessibleGroupBase {
   /**
    * {@inheritdoc}
    */
-  protected function doAccessCheck($operation, AccountInterface $account) {
-    $access = new AccessResultNeutral();
-    foreach ($this->dependencies as $dependency) {
-      $access = $access->orIf($dependency->access($operation, $account, TRUE));
-    }
-    return $access;
+  protected function doCombineAccess(AccessResultInterface $accumulatedAccess, AccessResultInterface $dependencyAccess) {
+    return $accumulatedAccess->orIf($dependencyAccess);
   }
 
 }
