9ca854ae10544624e001d669775c13cb3ab0fe4d
Ted Bowman <ted@tedbow.com>
2019-02-12 13:43:54 -0500

    clear contextual

--- core/modules/layout_builder/src/Element/LayoutBuilder.php
+++ core/modules/layout_builder/src/Element/LayoutBuilder.php
@@ -256,6 +256,7 @@ class LayoutBuilder extends RenderElement implements ContainerFactoryPluginInter
                 'delta' => $delta,
                 'region' => $region,
                 'uuid' => $uuid,
+                //'_time' => time(),
               ],
             ],
           ];
