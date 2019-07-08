2cb8f2cc7dc0cbe14658bb0c890d6fb949f70bf1
Ted Bowman <ted@tedbow.com>
2019-07-08 13:08:47 -0400

    add cases for different equals operators

--- core/tests/Drupal/Tests/Component/Version/ConstraintTest.php
+++ core/tests/Drupal/Tests/Component/Version/ConstraintTest.php
@@ -38,20 +38,20 @@ class ConstraintTest extends TestCase {
 
 
     // Alpha version.
-    $alpha = ['8.x-1.1-alpha12', '8.x'];
-    $tests['(8.x-1.1-alpha12)-alpha12'] = [$alpha, '1.1-alpha12', TRUE];
-    $tests['(8.x-1.1-alpha12)-alpha10'] = [$alpha, '1.1-alpha10', FALSE];
-    $tests['(8.x-1.1-alpha12)-beta1'] = [$alpha, '1.1-beta1', FALSE];
+    $alpha = ['==8.x-1.1-alpha12', '8.x'];
+    $tests['(==8.x-1.1-alpha12)-alpha12'] = [$alpha, '1.1-alpha12', TRUE];
+    $tests['(==8.x-1.1-alpha12)-alpha10'] = [$alpha, '1.1-alpha10', FALSE];
+    $tests['(==8.x-1.1-alpha12)-beta1'] = [$alpha, '1.1-beta1', FALSE];
 
     // Beta version.
-    $beta = ['8.x-1.1-beta8', '8.x'];
-    $tests['(8.x-1.1-beta8)-beta8'] = [$beta, '1.1-beta8', TRUE];
-    $tests['(8.x-1.1-beta8)-beta4'] = [$beta, '1.1-beta4', FALSE];
+    $beta = ['==8.x-1.1-beta8', '8.x'];
+    $tests['(==8.x-1.1-beta8)-beta8'] = [$beta, '1.1-beta8', TRUE];
+    $tests['(==8.x-1.1-beta8)-beta4'] = [$beta, '1.1-beta4', FALSE];
 
     // RC version.
-    $rc = ['8.x-1.1-rc11', '8.x'];
-    $tests['(8.x-1.1-rc11)-rc11'] = [$rc, '1.1-rc11', TRUE];
-    $tests['(8.x-1.1-rc11)-rc2'] = [$rc, '1.1-rc2', FALSE];
+    $rc = ['==8.x-1.1-rc11', '8.x'];
+    $tests['(==8.x-1.1-rc11)-rc11'] = [$rc, '1.1-rc11', TRUE];
+    $tests['(==8.x-1.1-rc11)-rc2'] = [$rc, '1.1-rc2', FALSE];
 
     // Test greater than.
     $greater = ['> 8.x-1.x', '8.x'];
@@ -87,11 +87,11 @@ class ConstraintTest extends TestCase {
 
     // Test greater than or equals and equals minor version. Both of these
     // conditions will pass.
-    $greater_and_equal_major = ['= 8.x-2.x, >= 2.4-alpha2', '8.x'];
-    $tests['(=8.x-2.x, >=2.4-alpha2)-8.x-2.4-beta3'] = [$greater_and_equal_major, '2.4-beta3', TRUE];
+    $greater_and_equal_major = ['== 8.x-2.x, >= 2.4-alpha2', '8.x'];
+    $tests['(== 8.x-2.x, >=2.4-alpha2)-8.x-2.4-beta3'] = [$greater_and_equal_major, '2.4-beta3', TRUE];
 
     // Test greater than  or equals and equals exact version.
-    $greater_and_equal_exact = ['= 8.x-2.0, >= 2.4-alpha2', '8.x'];
+    $greater_and_equal_exact = ['== 8.x-2.0, >= 2.4-alpha2', '8.x'];
     $tests['(=8.x-2.0, >=2.4-alpha2)-8.x-2.4-beta3'] = [$greater_and_equal_exact, '2.4-beta3', FALSE];
 
     // Test a nonsensical greater than and less than - no compatible versions.
@@ -109,13 +109,13 @@ class ConstraintTest extends TestCase {
     $tests['(<8.x-4.x,>8.x-1.x)-1.9-7.x'] = [$less_and_greater, '1.9', FALSE];
 
     // Test 2 equals with 1 that matching and with nonsensical missing a dash.
-    $tests['(=8.x2.x,=2.4-beta3)-2.4-beta3'] = [['= 8.x2.x, = 2.4-beta3', '8.x'], '2.4-beta3', FALSE];
+    $tests['(=8.x2.x,=2.4-beta3)-2.4-beta3'] = [['== 8.x2.x, == 2.4-beta3', '8.x'], '2.4-beta3', FALSE];
 
     // Test with a missing dash.
-    $tests['(= 8.x2)-8.x'] = [['= 8.x2', '8.x'], '8.x', TRUE];
+    $tests['(== 8.x2)-8.x'] = [['== 8.x2', '8.x'], '8.x', TRUE];
 
     // Test multiple equals which will always be false.
-    $equals_x3 = ['= 8.x-2.1, = 8.x-2.3, 8.x.2.5', '8.x'];
+    $equals_x3 = ['== 8.x-2.1, == 8.x-2.3, 8.x.2.5', '8.x'];
     $tests['(=8.x-2.1,=8.x-2.3,8.x.2.5)-2.1'] = [$equals_x3, '2.1', FALSE];
     $tests['(=8.x-2.1,=8.x-2.3,8.x.2.5)-2.1'] = [$equals_x3, '2.2', FALSE];
 
@@ -130,20 +130,33 @@ class ConstraintTest extends TestCase {
     $tests['(>1.0, <=3.2, !=3.0)-3.3'] = [$greater_less_not_exact, '3.3', FALSE];
 
     $original_tests = $tests;
-    foreach ($tests as $key => $test) {
-      if (strpos($test[0][0], ' ') !== FALSE) {
-        $tests["$key-no-space"] = $test;
-        $tests["$key-no-space"][0][0] = str_replace(' ', '', $test[0][0]);
+    // One or more spaces are supported by not require between commas and
+    // version numbers and between operators and version numbers.
+    foreach ($original_tests as $key => $original_test) {
+      if (strpos($original_test[0][0], ' ') !== FALSE) {
+        $tests["$key-no-space"] = $original_test;
+        $tests["$key-no-space"][0][0] = str_replace(' ', '', $original_test[0][0]);
 
-        $tests["$key-extra-space"] = $test;
-        $tests["$key-extra-space"][0][0] = str_replace(' ', '     ', $test[0][0]);
+        $tests["$key-extra-space"] = $original_test;
+        $tests["$key-extra-space"][0][0] = str_replace(' ', '     ', $original_test[0][0]);
       }
     }
+    // != and <> are both supported.
     foreach ($original_tests as $key => $original_test) {
       if (strpos($original_test[0][0], '!=') !== FALSE) {
         $new_key = str_replace('!=', '<>', $key);
         $tests[$new_key] = $original_test;
         $tests[$new_key][0][0] = str_replace('!=', '<>', $original_test[0][0]);
+      }
+    }
+    // ==, =, and no operator are all support for equals.
+    foreach ($original_tests as $key => $original_test) {
+      foreach (['=', ''] as $equal_operator) {
+        if (strpos($original_test[0][0], '==') !== FALSE) {
+          $new_key = str_replace('==', $equal_operator, $key);
+          $tests[$new_key] = $original_test;
+          $tests[$new_key][0][0] = str_replace('==', $equal_operator, $original_test[0][0]);
+        }
       }
     }
     return $tests;
