4561f74db3ba6c50cf8aa708d46fc3e767a48413
Ted Bowman <ted@tedbow.com>
2019-11-27 12:59:03 -0500

    change to getVersionStringWithoutCoreCompatibility

--- core/modules/update/src/ModuleVersionParser.php
+++ core/modules/update/src/ModuleVersionParser.php
@@ -31,7 +31,7 @@ class ModuleVersionParser {
    *   The major version.
    */
   public function getMajorVersion() {
-    return explode('.', $this->getVersionString())[0];
+    return explode('.', $this->getVersionStringWithoutCoreCompatibility())[0];
   }
 
   /**
@@ -41,7 +41,7 @@ class ModuleVersionParser {
    *   The minor version if available otherwise NULL.
    */
   public function getMinorVersion() {
-    $version_parts = explode('.', $this->getVersionString());
+    $version_parts = explode('.', $this->getVersionStringWithoutCoreCompatibility());
     return count($version_parts) === 2 ? NULL : $version_parts[1];
   }
 
@@ -52,7 +52,7 @@ class ModuleVersionParser {
    *   The patch version.
    */
   public function getPatchVersion() {
-    $version_parts = explode('.', $this->getVersionString());
+    $version_parts = explode('.', $this->getVersionStringWithoutCoreCompatibility());
     $last_version_part = count($version_parts) === 2 ? $version_parts[1] : $version_parts[2];
     return explode('-', $last_version_part)[0];
   }
@@ -63,9 +63,8 @@ class ModuleVersionParser {
    * @return string
    *   The version string.
    */
-  private function getVersionString() {
-    $original_version = $this->version;
-    $version = strpos($original_version, '8.x-') === 0 ? str_replace('8.x-', '', $original_version) : $original_version;
+  private function getVersionStringWithoutCoreCompatibility() {
+    $version = strpos($this->version, '8.x-') === 0 ? str_replace('8.x-', '', $this->version) : $this->version;
     return $version;
   }
 
@@ -76,7 +75,7 @@ class ModuleVersionParser {
    *   The version extra string if available otherwise NULL.
    */
   public function getVersionExtra() {
-    $version_parts = explode('.', $this->getVersionString());
+    $version_parts = explode('.', $this->getVersionStringWithoutCoreCompatibility());
     $last_version_parts = explode('-', count($version_parts) === 2 ? $version_parts[1] : $version_parts[2]);
     return count($last_version_parts) === 1 ? NULL : $last_version_parts[1];
   }
