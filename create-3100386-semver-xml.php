c605c48f1dfeffd190684c1100398fb6139ef1c0
Ted Bowman <ted@tedbow.com>
2020-03-27 13:53:44 -0400

    add releases via script

--- create-3100386-semver-xml.php
+++ create-3100386-semver-xml.php
@@ -17,7 +17,10 @@ foreach ($xml_files as $xml_file) {
   }
   $contents = transformContents($contents);
 
-  if ($new_file_name === '')
+  if (strpos($new_file_name, 'semver_test.1.0.xml') !== FALSE) {
+    $contents = addReleases($contents);
+
+  }
   //print "$new_file_name\n";
   file_put_contents($new_file_name, $contents);
 }
@@ -58,4 +61,109 @@ function transformContents($contents) {
   $contents = (string) $xml->asXML();
 
   return $contents;
+}
+
+
+function  addReleases($contents) {
+  $releases = <<<RELEASES
+<?xml version="1.0" encoding="utf-8"?>
+<releases>
+<release>
+    <name>Semver Test 8.x-7.1</name>
+    <version>8.x-7.1</version>
+    <tag>SEMVER_TEST-8-x-7-1</tag>
+    <status>published</status>
+    <release_link>http://example.com/semver_test-8-x-7-1-release</release_link>
+    <download_link>http://example.com/semver_test-8-x-7-1.tar.gz</download_link>
+    <date>1250424521</date>
+    <terms>
+      <term><name>Release type</name><value>New features</value></term>
+      <term><name>Release type</name><value>Bug fixes</value></term>
+    </terms>
+  </release>
+  <release>
+    <name>Semver Test 8.x-7.1-beta1</name>
+    <version>8.x-7.1-beta1</version>
+    <tag>SEMVER_TEST-8-x-7-1-beta1</tag>
+    <status>published</status>
+    <release_link>http://example.com/semver_test-8-x-7-1-beta1-release</release_link>
+    <download_link>http://example.com/semver_test-8-x-7-1-beta1.tar.gz</download_link>
+    <date>1250424521</date>
+    <terms>
+      <term><name>Release type</name><value>New features</value></term>
+      <term><name>Release type</name><value>Bug fixes</value></term>
+    </terms>
+  </release>
+  <release>
+    <name>Semver Test 8.x-7.1-alpha1</name>
+    <version>8.x-7.1-alpha1</version>
+    <tag>SEMVER_TEST-8-x-7-1-alpha1</tag>
+    <status>published</status>
+    <release_link>http://example.com/semver_test-8-x-7-1-alpha1-release</release_link>
+    <download_link>http://example.com/semver_test-8-x-7-1-alpha1.tar.gz</download_link>
+    <date>1250424521</date>
+    <terms>
+      <term><name>Release type</name><value>New features</value></term>
+      <term><name>Release type</name><value>Bug fixes</value></term>
+    </terms>
+  </release>
+  <release>
+    <name>Semver Test 8.x-7.0</name>
+    <version>8.x-7.0</version>
+    <tag>SEMVER_TEST-8-x-7-0</tag>
+    <status>published</status>
+    <release_link>http://example.com/semver_test-8-x-7-0-release</release_link>
+    <download_link>http://example.com/semver_test-8-x-7-0.tar.gz</download_link>
+    <date>1250424521</date>
+    <terms>
+      <term><name>Release type</name><value>New features</value></term>
+      <term><name>Release type</name><value>Bug fixes</value></term>
+    </terms>
+  </release>
+  <release>
+    <name>Semver Test 8.x-7.0-beta1</name>
+    <version>8.x-7.0-beta1</version>
+    <tag>SEMVER_TEST-8-x-7-0-beta1</tag>
+    <status>published</status>
+    <release_link>http://example.com/semver_test-8-x-7-0-beta1-release</release_link>
+    <download_link>http://example.com/semver_test-8-x-7-0-beta1.tar.gz</download_link>
+    <date>1250424521</date>
+    <terms>
+      <term><name>Release type</name><value>New features</value></term>
+      <term><name>Release type</name><value>Bug fixes</value></term>
+    </terms>
+  </release>
+  <release>
+    <name>Semver Test 8.x-7.0-alpha1</name>
+    <version>8.x-7.0-alpha1</version>
+    <tag>SEMVER_TEST-8-x-7-0-alpha1</tag>
+    <status>published</status>
+    <release_link>http://example.com/semver_test-8-x-7-0-alpha1-release</release_link>
+    <download_link>http://example.com/semver_test-8-x-7-0-alpha1.tar.gz</download_link>
+    <date>1250424521</date>
+    <terms>
+      <term><name>Release type</name><value>New features</value></term>
+      <term><name>Release type</name><value>Bug fixes</value></term>
+    </terms>
+</release>
+</releases>
+RELEASES;
+  $doc = new DOMDocument();
+  $doc->loadXML($contents);
+  $element = $doc->getElementsByTagName('releases')->item(0);
+  $releases_new = new DOMDocument();
+  $releases_new->loadXML($releases);
+  foreach ($releases_new->getElementsByTagName('release') as $new_release) {
+    $imported = $doc->importNode($new_release, TRUE);
+    $element->appendChild($imported);
+  }
+
+  $contents =  $doc->saveXML();
+
+  $xml = new SimpleXMLElement($contents);
+  $xml->supported_branches = "8.x-7.,8.0.,8.1.";
+
+
+
+  return (string) $xml->asXML();;
 }
