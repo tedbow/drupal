4b93e407f4f32c8e20755647cb17711257f7b13c
Ted Bowman <ted@tedbow.com>
2020-09-01 16:02:04 -0400

    move to 1 psa test class

--- core/modules/update/tests/src/Functional/PsaTest.php
+++ core/modules/update/tests/src/Functional/PsaTest.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tests\update\Functional;
 
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Test\AssertMailTrait;
 use Drupal\Core\Url;
 use Drupal\Tests\BrowserTestBase;
 
@@ -11,6 +13,9 @@ use Drupal\Tests\BrowserTestBase;
  * @group update
  */
 class PsaTest extends BrowserTestBase {
+
+  use AssertMailTrait;
+  use StringTranslationTrait;
 
   /**
    * {@inheritdoc}
@@ -85,6 +90,42 @@ class PsaTest extends BrowserTestBase {
     $this->assertSession()->pageTextNotContains('Critical Release - PSA-2019-02-19');
     $this->drupalGet(Url::fromRoute('system.status'));
     $this->assertSession()->pageTextNotContains('urgent announcements require your attention');
+  }
+
+  /**
+   * Tests sending PSA email notifications.
+   */
+  public function testPsaMail() {
+    // Setup test PSA endpoint.
+    $end_point = $this->buildUrl(Url::fromRoute('psa_test.json_test_controller'));
+    $this->config('update.settings')
+      ->set('psa.endpoint', $end_point)
+      ->save();
+    // Setup a default destination email address.
+    $this->config('update.settings')
+      ->set('notification.emails', ['admin@example.com'])
+      ->save();
+
+
+    // Test PSAs on admin pages.
+    $this->drupalGet(Url::fromRoute('system.admin'));
+    $this->assertSession()->pageTextContains('Critical Release - SA-2019-02-19');
+
+    // Email should be sent.
+    $notify = $this->container->get('update.psa_notify');
+    $notify->send();
+    $this->assertCount(1, $this->getMails());
+    $this->assertMailString('subject', '3 urgent Drupal announcements require your attention', 1);
+    $this->assertMailString('body', 'Critical Release - SA-2019-02-19', 1);
+
+    // No email should be sent if PSA's are disabled.
+    $this->container->get('state')->set('system.test_mail_collector', []);
+    $this->container->get('state')->delete('update_psa.notify_last_check');
+    $this->config('update.settings')
+      ->set('psa.enable', FALSE)
+      ->save();
+    $notify->send();
+    $this->assertCount(0, $this->getMails());
   }
 
 }
