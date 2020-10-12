2347ea74b33a033895e845c88f32ea3ceecfebde
Ted Bowman <ted@tedbow.com>
2020-10-12 14:29:56 -0400

    add contrib cases

--- core/modules/update/tests/src/Kernel/PsaFetcherTest.php
+++ core/modules/update/tests/src/Kernel/PsaFetcherTest.php
@@ -34,17 +34,16 @@ class PsaFetcherTest extends KernelTestBase {
   /**
    * Tests contrib advisories that should be displayed.
    *
-   * @param string $existing_version
+   * @param mixed[] $feed_item
+   *   The feed item to test. 'title' and 'link' are omitted from this array
+   *   because they do not need to vary between test cases.
+   * @param string|null $existing_version
    *   The existing version of the module.
-   * @param int $is_psa
-   *   The 'is_psa' value for the feed item.
-   * @param array $insecure_versions
-   *   The 'insecure_versions' value for the feed item.
    *
    * @dataProvider providerShowAdvisories
    */
-  public function testShowContribAdvisories(string $existing_version, int $is_psa, array $insecure_versions): void {
-    $this->setProphesizedServices(FALSE, $is_psa, $insecure_versions, $existing_version);
+  public function testShowAdvisories(array $feed_item, string $existing_version = NULL): void {
+    $this->setProphesizedServices($feed_item, $existing_version);
     $fetcher = $this->container->get('update.psa_fetcher');
     /** @var \Drupal\Component\Render\FormattableMarkup[] $links */
     $links = $fetcher->getPublicServiceMessages();
@@ -53,69 +52,160 @@ class PsaFetcherTest extends KernelTestBase {
   }
 
   /**
-   * Dataprovider for testShowContribAdvisories().
+   * Dataprovider for testShowAdvisories().
    */
   public function providerShowAdvisories() {
     return [
-      'exact-non-psa' => [
+      'contrib:exact:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['8.x-1.0'],
+        ],
         'existing_version' => '8.x-1.0',
-        'is_psa' => 0,
-        'insecure_versions' => ['8.x-1.0'],
       ],
-      'not-exact-non-psa' => [
+      'contrib:not-exact:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['1.0'],
+        ],
         'existing_version' => '8.x-1.0',
-        'is_psa' => 0,
-        'insecure_versions' => ['1.0'],
       ],
-      'not-exact-non-psa-reversed' => [
+      'contrib:not-exact:non-psa-reversed' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['1.0'],
+        ],
         'existing_version' => '8.x-1.0',
-        'is_psa' => 0,
-        'insecure_versions' => ['1.0'],
       ],
-      'semver-non-psa' => [
+      'contrib:semver:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['1.0.0'],
+        ],
         'existing_version' => '1.0.0',
-        'is_psa' => 0,
-        'insecure_versions' => ['1.0.0'],
       ],
-      'semver-non-exact-non-psa' => [
+      'contrib:semver-non-exact:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['1.0'],
+        ],
         'existing_version' => '1.0.0',
-        'is_psa' => 0,
-        'insecure_versions' => ['1.0'],
       ],
-      'semver-major-match-non-psa' => [
+      'contrib:semver-major-match:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['1.0'],
+        ],
         'existing_version' => '1.0.0',
-        'is_psa' => 0,
-        'insecure_versions' => ['8.x-1.0'],
       ],
-      'existing-extra-non-psa' => [
+      'contrib:existing-extra:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['8.x-1.0'],
+        ],
         'existing_version' => '8.x-1.0-extraStringNotSpecial',
-        'is_psa' => 0,
-        'insecure_versions' => ['8.x-1.0'],
+
       ],
-      'both-extra-non-psa' => [
+      'contrib:both-extra:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['8.x-1.0-extraStringNotSpecial'],
+        ],
         'existing_version' => '8.x-1.0-alsoNotSpecialNotMatching',
-        'is_psa' => 0,
-        'insecure_versions' => ['8.x-1.0-extraStringNotSpecial'],
       ],
-      'exact-psa' => [
+      'contrib:exact:psa' => [
+        'feed_item' => [
+          'is_psa' => 1,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['8.x-1.0'],
+        ],
         'existing_version' => '8.x-1.0',
-        'is_psa' => 1,
-        'insecure_versions' => ['8.x-1.0'],
       ],
-      'not-exact-psa' => [
+      'contrib:not-exact:psa' => [
+        'feed_item' => [
+          'is_psa' => 1,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['8.x-1.0'],
+        ],
         'existing_version' => '8.x-1.0',
-        'is_psa' => 1,
-        'insecure_versions' => ['1.0'],
+
       ],
-      'non-matching-psa' => [
+      'contrib:non-matching:psa' => [
+        'feed_item' => [
+          'is_psa' => 1,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['8.x-1.0'],
+        ],
         'existing_version' => '8.x-2.0',
-        'is_psa' => 1,
-        'insecure_versions' => ['8.x-1.0'],
       ],
-      'no-insecure-psa' => [
+      'contrib:no-insecure:psa' => [
+        'feed_item' => [
+          'is_psa' => 1,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => [],
+        ],
         'existing_version' => '8.x-2.0',
-        'is_psa' => 1,
-        'insecure_versions' => [],
+      ],
+      'core:exact:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'core',
+          'project' => 'drupal',
+          'insecure' => [\Drupal::VERSION],
+        ],
+      ],
+      'core:exact:psa' => [
+        'feed_item' => [
+          'is_psa' => 1,
+          'type' => 'core',
+          'project' => 'drupal',
+          'insecure' => [\Drupal::VERSION],
+        ],
+      ],
+      'core:not-exact:psa' => [
+        'feed_item' => [
+          'is_psa' => 1,
+          'type' => 'core',
+          'project' => 'drupal',
+          'insecure' => ['9.1'],
+        ],
+
+      ],
+      'core:non-matching:psa' => [
+        'feed_item' => [
+          'is_psa' => 1,
+          'type' => 'core',
+          'project' => 'drupal',
+          'insecure' => ['9.0.0'],
+        ],
+      ],
+      'core:no-insecure:psa' => [
+        'feed_item' => [
+          'is_psa' => 1,
+          'type' => 'core',
+          'project' => 'drupal',
+          'insecure' => [],
+        ],
       ],
     ];
   }
@@ -123,17 +213,16 @@ class PsaFetcherTest extends KernelTestBase {
   /**
    * Tests Advisories that should be ignored.
    *
-   * @param string $existing_version
+   * @param mixed[] $feed_item
+   *   The feed item to test. 'title' and 'link' are omitted from this array
+   *   because they do not need to vary between test cases.
+   * @param string|null $existing_version
    *   The existing version of the module.
-   * @param int $is_psa
-   *   The 'is_psa' value for the feed item.
-   * @param array $insecure_versions
-   *   The 'insecure_versions' value for the feed item.
    *
-   * @dataProvider providerIgnoreContribAdvisories
+   * @dataProvider providerIgnoreAdvisories
    */
-  public function testIgnoreContribAdvisories(bool $is_core, int $is_psa, array $insecure_versions, string $existing_version): void {
-    $this->setProphesizedServices($is_core, $is_psa, $insecure_versions, $existing_version);
+  public function testIgnoreAdvisories(array $feed_item, string $existing_version = NULL): void {
+    $this->setProphesizedServices($feed_item, $existing_version);
     $fetcher = $this->container->get('update.psa_fetcher');
     /** @var \Drupal\Component\Render\FormattableMarkup[] $links */
     $links = $fetcher->getPublicServiceMessages();
@@ -141,91 +230,140 @@ class PsaFetcherTest extends KernelTestBase {
   }
 
   /**
-   * Dataprovider for testIgnoreContribAdvisories().
+   * Dataprovider for testIgnoreAdvisories().
    */
-  public function providerIgnoreContribAdvisories() {
+  public function providerIgnoreAdvisories() {
     return [
-      'contrib:non-matching-non-psa' => [
-        'is_core' => FALSE,
-        'is_psa' => 0,
-        'insecure_versions' => ['8.x-1.1'],
+      'contrib:non-matching:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['8.x-1.1'],
+        ],
         'existing_version' => '8.x-1.0',
       ],
-      'contrib:non-matching-not-exact-non-psa' => [
-        'is_core' => FALSE,
-        'is_psa' => 0,
-        'insecure_versions' => ['1.1'],
+      'contrib:non-matching-not-exact:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['1.1'],
+        ],
         'existing_version' => '8.x-1.0',
       ],
-      'contrib:semver-7major-match-non-psa' => [
-        'is_core' => FALSE,
-        'is_psa' => 0,
-        'insecure_versions' => ['7.x-1.0'],
+      'contrib:semver-7major-match:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['7.x-1.0'],
+        ],
         'existing_version' => '1.0.0',
       ],
-      'contrib:semver-different-majors-non-psa' => [
-        'is_core' => FALSE,
-        'is_psa' => 0,
-        'insecure_versions' => ['7.x-1.0'],
+      'contrib:semver-different-majors:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['7.x-1.0'],
+        ],
         'existing_version' => '8.x-1.0',
       ],
-      'contrib:no-version-non-psa' => [
-        'is_core' => FALSE,
-        'is_psa' => 0,
-        'insecure_versions' => ['8.x-1.1'],
+      'contrib:no-version:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['8.x-1.1'],
+        ],
         'existing_version' => '',
       ],
-      'contrib:insecure-extra-non-psa' => [
-        'is_core' => FALSE,
-        'is_psa' => 0,
-        'insecure_versions' => ['8.x-1.0-extraStringNotSpecial'],
+      'contrib:insecure-extra:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['8.x-1.0-extraStringNotSpecial'],
+        ],
         'existing_version' => '8.x-1.0',
       ],
-      'contrib:insecure-dev-non-psa' => [
-        'is_core' => FALSE,
-        'is_psa' => 0,
-        'insecure_versions' => ['8.x-1.0-dev'],
+      'contrib:insecure-dev:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['8.x-1.0-dev'],
+        ],
         'existing_version' => '8.x-1.0',
       ],
-      'contrib:exiting-dev-non-psa' => [
-        'is_core' => FALSE,
-        'is_psa' => 0,
-        'insecure_versions' => ['8.x-1.0'],
+      'contrib:exiting-dev:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'the_project',
+          'insecure' => ['8.x-1.0'],
+        ],
         'existing_version' => '8.x-1.0-dev',
+      ],
+      'contrib:non-existing-project:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'module',
+          'project' => 'different_project',
+          'insecure' => ['8.x-1.0'],
+        ],
+      ],
+      'contrib:non-existing-project:psa' => [
+        'feed_item' => [
+          'is_psa' => 1,
+          'type' => 'module',
+          'project' => 'different_project',
+          'insecure' => ['8.x-1.0'],
+        ],
+      ],
+      'core:non-matching:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'core',
+          'project' => 'drupal',
+          'insecure' => ['9.0.0'],
+        ],
+      ],
+      'core:non-matching-not-exact:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'core',
+          'project' => 'drupal',
+          'insecure' => ['9.1'],
+        ],
+      ],
+      'core:no-insecure:non-psa' => [
+        'feed_item' => [
+          'is_psa' => 0,
+          'type' => 'core',
+          'project' => 'drupal',
+          'insecure' => [],
+        ],
       ],
     ];
   }
 
-  public function testCoreShowAdvisories() {
-    $this->setProphesizedServices(TRUE, $is_psa, $insecure_versions, $existing_version);
-    $fetcher = $this->container->get('update.psa_fetcher');
-    /** @var \Drupal\Component\Render\FormattableMarkup[] $links */
-    $links = $fetcher->getPublicServiceMessages();
-    static::assertCount(1, $links);
-    $this->assertSame('<a href="http://thesa.com">SA title</a>', (string) $links[0]);
-  }
   /**
    * Sets the 'http_client' and 'extension.list.module' services for the tests.
    *
-   * @param bool $is_core
-   *   Whether the SA item is for Drupal core.
-   * @param int $is_psa
-   *   The 'is_psa' value for the feed item.
-   * @param array $insecure_versions
-   *   The 'insecure_versions' value for the feed item.
+   * @param mixed[] $feed_item
+   *   The feed item to test. 'title' and 'link' are omitted from this array
+   *   because they do not need to vary between test cases.
    * @param string|null $existing_version
    *   The existing version of the module.
    */
-  protected function setProphesizedServices(bool $is_core, int $is_psa, array $insecure_versions, string $existing_version = NULL): void {
-    $sa = [
+  protected function setProphesizedServices(array $feed_item, string $existing_version = NULL): void {
+    $feed_item += [
       'title' => 'SA title',
-      'project' => ($is_core ? 'drupal' : 'the_project'),
-      'type' => ($is_core ? 'core' : 'module'),
       'link' => 'http://thesa.com',
-      'insecure' => $insecure_versions,
-      'is_psa' => $is_psa,
     ];
-    $json_string = json_encode([$sa]);
+    $json_string = json_encode([$feed_item]);
     $stream = $this->prophesize(StreamInterface::class);
     $stream->__toString()->willReturn($json_string);
     $response = $this->prophesize(ResponseInterface::class);
@@ -246,7 +384,6 @@ class PsaFetcherTest extends KernelTestBase {
 
       $this->container->set('extension.list.module', $module_list->reveal());
     }
-
   }
 
 }
